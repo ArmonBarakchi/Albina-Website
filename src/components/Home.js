73cd0c8f3df2f687910d117daeca77854a0895d8
Armon <abarakchi@ucsd.edu>
2026-04-23 10:06:46 -0700

    Updated Resume

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,6 @@
 // src/components/Home.js
 import React from "react";
 import Hero from "./Hero";
-import Guides from "./Guides";
-import Blogs from "./Blogs";
 import Classes from "./Classes"
 import "./Home.css";
 
